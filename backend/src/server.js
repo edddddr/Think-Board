8b53a24972d68e20df40cf97f016edf18705ac77
edddddr <endt6342@gmail.com>
2025-12-01 03:54:24 -0900

    Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,21 +5,32 @@ import dotenv from 'dotenv';
 import rateLimiter from "./middlelware/ratelimiter.js";
 import cors from 'cors'
 
-dotenv.config();
+import path from 'path'
 
+dotenv.config();
 
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
-app.use(cors())
+if(process.env.NODE_ENV !== 'production'){
+    app.use(cors())
+}
 
 app.use(express.json())
 app.use(rateLimiter)
 
-
 app.use("/api/notes", notesRouter)
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    
+    app.get("*",(req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+
+})
+}
 
 
 connectDB().then(() => {
